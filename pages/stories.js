00987f7789adf3cc1913e904dc39af890e4d3e27
Germano Lira <dolan.3@hotmail.com>
2020-12-02 23:46:22 -0300

    Testando o NextJS

---
+++ pages/stories.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+function Stories() {
+    return(
+        <div>
+            <h1>Stories</h1>
+
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </div>
+        )
+}
+  
+export default Stories
